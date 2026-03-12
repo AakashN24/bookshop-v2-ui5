70102be4b43d1777bde017d145e1705cc3c088c2
Aakash Nambaru <aakash112541@gmail.com>
2026-03-12 07:57:36 +0000

    added update functionality

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,8 +1,10 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
-    "bookshopcatalogv2/model/models"
-], (Controller, MessageToast, models) => {
+    "bookshopcatalogv2/model/models",
+    "sap/ui/model/Context",
+    "sap/m/MessageBox"
+], (Controller, MessageToast, models, Context) => {
     "use strict";
 
     return Controller.extend("bookshopcatalogv2.controller.View1", {
@@ -15,9 +17,20 @@ sap.ui.define([
             this.oCreateDialog.open();
             MessageToast.show('Create Clicked');
         },
+        async onEditPress(oEvent) {
+            const oModel = this.getView().getModel();
+            const sPath = oEvent.getSource().getBindingContext().getPath();
+            this.oEditDialog ??= await this.loadFragment({
+                name: "bookshopcatalogv2.view.fragments.EditBook"
+            })
+            this.oEditDialog.setBindingContext(new Context(oModel, sPath))
+            this.oEditDialog.open();
+        },
+        //for create dialog
         onDialogCancel() {
             this.oCreateDialog?.close();
         },
+        //for create dialog
         onDialogCreate() {
             let oPayload = this.getView().getModel("dialogInput").getData();
 
@@ -38,9 +51,11 @@ sap.ui.define([
                     }
                 })
         },
+        //for create dialog
         onAfterCloseDialog() {
             this.getOwnerComponent().setModel(models.createDialogInputModel(), "dialogInput");
         },
+        //for deleting the records
         onDeletePress(oEvent) {
             let oContext = oEvent.getSource().getBindingContext();
 
@@ -55,6 +70,28 @@ sap.ui.define([
                     console.log(oError);
                 }
             })
+        },
+        //for edit dialog
+        onPressCancelBookDialog() {
+            this.oEditDialog.close();
+        },
+        onPressUpdateBookDialog() {
+            const oModel = this.getView().getModel();
+            const sPath = this.oEditDialog.getBindingContext().getPath();
+            const oPayload = {
+                title: this.byId("edit_input1").getValue(),
+                stock: this.byId("edit_stockInput").getValue()
+            }
+            oModel.update(sPath, oPayload, {
+                success: (oData, oResponse) => {
+                    MessageToast.show("Successfully Updated")
+                    this.oEditDialog.close();
+                },
+                error: (oError) => {
+                    console.log('error---', oError)
+                    // MessageToast.show()
+                }
+            }).bind(this);
         }
     });
 });
